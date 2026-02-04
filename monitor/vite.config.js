7c798a931e922625aced7255dab4b9b7a412dd97
romita <romita_p@pursuitsoftware.com>
2026-02-04 12:25:56 -0500

    dashboard updates

--- monitor/vite.config.js
+++ monitor/vite.config.js
@@ -3,4 +3,8 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    host: true,   // allows other devices on LAN
+    port: 5173
+  }
 });
